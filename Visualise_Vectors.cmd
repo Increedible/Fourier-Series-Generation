621d60a7b308bc5ee44f642180bb7858bc717735
Increedible <108118527+Increedible@users.noreply.github.com>
2025-09-07 02:40:49 +0200

    Updated version

---
+++ Visualise_Vectors.cmd
@@ -0,0 +1,7 @@
+
+@echo off
+rem This script was created by Nuitka to execute 'Visualise_Vectors.exe' with Python DLL being found.
+set PATH=C:\PROGRA~1\Python38;%PATH%
+set PYTHONHOME=C:\Program Files\Python38
+set NUITKA_PYTHONPATH=C:\Users\mrinc\Desktop\Python\fourier renderer;C:\Program Files\Python38\DLLs;C:\Program Files\Python38\lib;C:\Program Files\Python38;C:\Users\mrinc\Desktop\Python\venv;C:\Users\mrinc\Desktop\Python\venv\lib\site-packages;C:\Users\mrinc\Desktop\Python\venv\lib\site-packages\win32;C:\Users\mrinc\Desktop\Python\venv\lib\site-packages\win32\lib;C:\Users\mrinc\Desktop\Python\venv\lib\site-packages\Pythonwin
+"%~dp0Visualise_Vectors.exe" %*
